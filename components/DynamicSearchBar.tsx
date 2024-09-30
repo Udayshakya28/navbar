2ec34ffb97f7963c2cbfd6416127ab3cd695e261
Udayshakya28 <ushakya82@gmail.com>
2024-09-30 22:32:58 +0530

    chnages added

--- components/DynamicSearchBar.tsx
+++ components/DynamicSearchBar.tsx
@@ -5,9 +5,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 
 import { addDays, format, set } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
 import { DateRange } from "react-date-range";
-import "react-date-range/dist/styles.css"; 
+import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 
 
@@ -27,22 +26,21 @@ const locations = [
     'uttar pradesh ',
     'haryana',
     'west bengal',
-
-
 ];
 
 
 export default function DynamicSearchBar() {
     const [location, setLocation] = useState('Anywhere');
-    const [checkInDates, setCheckInDates] = useState('Any week');
-    const [checkOutDates, setCheckOutDates] = useState('Any week');
-    const [guests, setGuests] = useState(0);
-
-    const [adultsCount, setAdultsCount] = useState(1);
+    const [checkIn, setcheckIn] = useState('Any week');
+    const [adultCnt, setadultCnt] = useState(1);
     const [childrenCount, setChildrenCount] = useState(0);
     const [petsCount, setPetsCount] = useState(0);
     const [totalGuests, setTotalGuests] = useState(1);
-   
+    const [checkOutDates, setCheckOutDates] = useState('Any week');
+    const [guests, setGuests] = useState(0);
+
+
+
     const formatToYYYYMMDD = (date: any) => {
         const months = [
             "Jan", "Feb", "Mar", "Apr", "May", "Jun",
@@ -63,18 +61,18 @@ export default function DynamicSearchBar() {
             key: "selection",
         },
     ]);
-    const onSelectDate = (ranges: any) => {
+    const onDateSelection = (ranges: any) => {
         const formattedStartDate = formatToYYYYMMDD(ranges.selection.startDate);
         const formattedEndDate = formatToYYYYMMDD(ranges.selection.endDate);
-        setCheckInDates(formattedStartDate);
+        setcheckIn(formattedStartDate);
         setCheckOutDates(formattedEndDate)
         const formattedDateRange = [formattedStartDate, formattedEndDate];
-        
+
 
         setSelectedDateRange([ranges.selection]);
     };
 
-    const [searchBarActive, setSearchBarActive] = useState(false);
+    const [isActive, setisActive] = useState(false);
 
 
 
@@ -83,15 +81,15 @@ export default function DynamicSearchBar() {
         const excludeElement = event.target.closest('.exclude-class');
 
         if (!searchBarElement && !excludeElement) {
-           
-            setSearchBarActive(false);
+
+            setisActive(false);
         } else if (searchBarElement) {
-           
-            setSearchBarActive(true);
+
+            setisActive(true);
         }
     };
 
-    
+
     useEffect(() => {
         document.addEventListener('mousedown', handleClickOutside);
         return () => {
@@ -99,33 +97,32 @@ export default function DynamicSearchBar() {
         };
     }, []);
 
-    
-    const [query, setQuery] = useState(''); 
-    const [filteredLocations, setFilteredLocations] = useState([]); 
 
-   
+    const [query, setQuery] = useState('');
+    const [filteredLocations, setFilteredLocations] = useState([]);
+
+
     const handleLocSearch = (e: any) => {
         const value = e.target.value;
         setQuery(value);
 
-       
+
         const filtered: any = locations.filter((location: any) =>
             location.toLowerCase().includes(value.toLowerCase())
         );
         setFilteredLocations(filtered);
     };
 
-    
+
     const handleSelectLocation = (location: any) => {
         setQuery(location);
         setLocation(location)
-      
-        setFilteredLocations([]); 
-        setSearchModal(null); 
+
+        setFilteredLocations([]);
+        setDialouge("");
     };
 
-    const scrollTimer = useRef<NodeJS.Timeout | null>(null);
-    const lastScrollY = useRef(window.scrollY);
+
     const router = useRouter();
 
 
@@ -133,21 +130,21 @@ export default function DynamicSearchBar() {
     const incrementCount = (type: string) => {
         if (type === 'adults') {
             setTotalGuests(totalGuests + 1);
-            setAdultsCount(adultsCount + 1);
+            setadultCnt(adultCnt + 1);
         } else if (type === 'children') {
             setChildrenCount(childrenCount + 1);
             setTotalGuests(totalGuests + 1);
         } else if (type === 'pets') {
             setPetsCount(petsCount + 1);
-        
+
         }
     };
 
 
     const decrementCount = (type: string) => {
-        if (type === 'adults' && adultsCount > 0) {
-            setAdultsCount(adultsCount - 1);
-            setTotalGuests(Math.max(totalGuests - 1, 0)); 
+        if (type === 'adults' && adultCnt > 0) {
+            setadultCnt(adultCnt - 1);
+            setTotalGuests(Math.max(totalGuests - 1, 0));
         } else if (type === 'children' && childrenCount > 0) {
             setChildrenCount(childrenCount - 1);
             setTotalGuests(Math.max(totalGuests - 1, 0));
@@ -160,25 +157,24 @@ export default function DynamicSearchBar() {
 
     const handleSearch = () => {
         const Location = location.trim();
-        const checkInDates = selectedDateRange[0]?.startDate?.toDateString() || 'Any week';
+        const checkIn = selectedDateRange[0]?.startDate?.toDateString() || 'Any Week';
         const checkOutDates = selectedDateRange[0]?.endDate?.toDateString() || 'Any week';
-        const totalGuestsCount = adultsCount + childrenCount + petsCount;
-        const searchURL = `?location=${Location}&checkin=${checkInDates}&checkout=${checkOutDates}&adults=${adultsCount}&children=${childrenCount}&pets=${petsCount}&totalGuests=${totalGuestsCount}`;
+        const searchURL = `?location=${Location}&checkin=${checkIn}&adults=${adultCnt}&checkout=${checkOutDates}children=${childrenCount}&pets=${petsCount}`;
         router.push(searchURL);
-        setSearchBarActive(false);
-        setSearchModal("");
+        setisActive(false);
+        setDialouge("");
     };
 
-    const [searchModal, setSearchModal] = useState("")
+    const [searchModal, setDialouge] = useState("")
 
     return (
         <div className="dynamic-search-bar  z-[100] w-auto min-w-[min-content]   sm:max-w-full flex flex-col items-center transition-all duration-500">
             <div
                 onClick={handleClickOutside}
-                className={`w-full px-4 rounded-2xl flex flex-col justify-center items-center transition-all duration-500 ${searchBarActive ? 'h-[20vh]' : 'h-12'}`}
+                className={`w-full px-4 rounded-2xl flex flex-col justify-center items-center transition-all duration-500 ${isActive ? 'h-[20vh]' : 'h-12'}`}
             >
 
-                {searchBarActive && (
+                {isActive && (
                     <div className="mt-1 mb-3  w-[40%] flex justify-between  text-center">
                         <button className="w-full py-2 font-bold text-gray-900 hover:text-pink-500 transition-colors">
                             Stays
@@ -190,15 +186,13 @@ export default function DynamicSearchBar() {
                 )}
 
 
-                {searchBarActive ? (
+                {isActive ? (
 
                     <div className="  exclude-class bg-gray-200  h-[70px] flex flex-row items-center justify-between border rounded-full">
                         <div className="w-full">
                             <div className="flex flex-row items-center justify-between">
-
-                                {}
                                 <div
-                                    onClick={() => setSearchModal('location')}
+                                    onClick={() => setDialouge('location')}
                                     className={`cursor-pointer w-[200px] h-[54px] px-8 m-2 flex flex-col justify-center rounded-full hover:bg-opacity-70 ${searchModal === 'location'
                                         ? 'bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-white'
                                         : 'hover:bg-[#ffffff] hover:bg-opacity-80 '
@@ -257,7 +251,7 @@ export default function DynamicSearchBar() {
 
 
                                 <div
-                                    onClick={() => setSearchModal('checkin')}
+                                    onClick={() => setDialouge('checkin')}
                                     className={`cursor-pointer h-[54px] px-2 md:px-6 py-2 m-2 flex flex-col justify-center rounded-full ${searchModal === 'checkin'
                                         ? 'bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-white'
                                         : 'hover:bg-[#ffffff] hover:bg-opacity-80'
@@ -265,7 +259,7 @@ export default function DynamicSearchBar() {
                                 >
                                     <p className="text-xs font-semibold">Check in</p>
                                     <div className="flex flex-row items-center text-left text-[1rem] font-normal text-gray-400">
-                                        {}
+                                        { }
                                         {selectedDateRange[0].startDate ? (
                                             <>{format(selectedDateRange[0].startDate, 'LLL dd, y')}</>
                                         ) : (
@@ -274,9 +268,9 @@ export default function DynamicSearchBar() {
                                     </div>
                                 </div>
 
-                                {}
+                                { }
                                 <div
-                                    onClick={() => setSearchModal('checkout')}
+                                    onClick={() => setDialouge('checkout')}
                                     className={`cursor-pointer h-[54px] px-2 md:px-6 py-2 m-2 flex flex-col justify-center rounded-full ${searchModal === 'checkout'
                                         ? 'bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-white'
                                         : 'hover:bg-[#ffffff] hover:bg-opacity-90'
@@ -292,14 +286,14 @@ export default function DynamicSearchBar() {
                                     </div>
                                 </div>
 
-                                {}
+                                { }
                                 {(searchModal === 'checkout' || searchModal == "checkin") && (
                                     <div className="absolute top-[25vh] right-[20vw] border-[1px] mt-5 p-3 bg-white rounded-3xl flex justify-center">
                                         <DateRange
                                             rangeColors={['#f1424297']}
                                             ranges={selectedDateRange}
                                             date={new Date()}
-                                            onChange={onSelectDate}
+                                            onChange={onDateSelection}
                                             direction="horizontal"
                                             showDateDisplay={false}
                                             minDate={new Date()}
@@ -308,17 +302,17 @@ export default function DynamicSearchBar() {
                                         />
                                     </div>
                                 )}
-                                {}
+                                { }
 
-                                {}
+                                { }
                                 <div
-                                    onClick={() => setSearchModal('guests')}
+                                    onClick={() => setDialouge('guests')}
                                     className={` cursor-pointer h-[48px] gap-[10px] lg:h-[54px] px-2 md:px-6 py-2 m-2 flex flex-row justify-around items-center rounded-full ${searchModal === 'guests'
                                         ? 'bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:bg-white'
                                         : 'hover:bg-[#ffffff] hover:bg-opacity-90'
                                         }`}
                                 >
-                                    <div onClick={() => setSearchModal('guests')}>
+                                    <div onClick={() => setDialouge('guests')}>
                                         <p className="text-xs w-full font-semibold">Who</p>
                                         <div className="w-full flex flex-col items-center justify-start text-left text-[1rem] font-normal text-gray-400">
                                             {totalGuests} {totalGuests === 1 ? 'Guest' : 'Guests'}
@@ -327,11 +321,11 @@ export default function DynamicSearchBar() {
 
                                     {searchModal === 'guests' && (
                                         <div className="absolute top-[25vh] right-[20vw] border-[1px]   mt-5 p-4 rounded-3xl bg-white">
-                                            {}
+                                            { }
                                             <div className="mb-4 px-2 py-1 md:w-[400px] flex-row flex items-center justify-between border-gray-200">
                                                 <div className="flex items-center flex-col justify-between mb-2">
                                                     <span className="text-gray-900 font-semibold text-lg w-full text-left">Adults</span>
-                                                    {}
+                                                    { }
                                                 </div>
                                                 <div className="flex items-center gap-3">
                                                     <button
@@ -340,7 +334,7 @@ export default function DynamicSearchBar() {
                                                     >
                                                         <AiOutlineMinus className="h-5 w-5 text-gray-600" />
                                                     </button>
-                                                    <span className="text-md font-medium text-gray-700">{adultsCount}</span>
+                                                    <span className="text-md font-medium text-gray-700">{adultCnt}</span>
                                                     <button
                                                         className="p-3 rounded-full border border-gray-300"
                                                         onClick={() => incrementCount('adults')}
@@ -350,11 +344,11 @@ export default function DynamicSearchBar() {
                                                 </div>
                                             </div>
 
-                                            {}
+                                            { }
                                             <div className="mb-4 px-2 py-1 md:w-[400px] flex-row flex items-center justify-between border-gray-200">
                                                 <div className="flex items-center flex-col justify-between mb-2">
                                                     <span className="text-gray-900 font-semibold text-lg w-full text-left">Children</span>
-                                                    {}
+                                                    { }
                                                 </div>
                                                 <div className="flex items-center gap-3">
                                                     <button
@@ -373,11 +367,11 @@ export default function DynamicSearchBar() {
                                                 </div>
                                             </div>
 
-                                            {}
+                                            { }
                                             <div className="px-2 py-1 md:w-[400px] flex-row flex items-center justify-between border-gray-200">
                                                 <div className="flex items-center flex-col justify-between mb-2">
                                                     <span className="text-gray-900 font-semibold text-lg w-full text-left">Pets</span>
-                                                    {}
+                                                    { }
                                                 </div>
                                                 <div className="flex items-center gap-3">
                                                     <button
@@ -407,7 +401,7 @@ export default function DynamicSearchBar() {
                                     </div>
                                 </div>
 
-                                {}
+                                { }
 
                             </div>
                         </div>
@@ -422,7 +416,7 @@ export default function DynamicSearchBar() {
                             <div className="text-gray-900 font-semibold">{location}</div>
                         </div>
                         <div className="sm:flex justify-center items-center cursor-pointer px-4 h-full border-r-2">
-                            <div className="text-gray-900 font-semibold">{`${checkInDates} - ${checkOutDates}`}</div>
+                            <div className="text-gray-900 font-semibold">{`${checkIn} - ${checkOutDates}`}</div>
                         </div>
                         <div className=" sm:flex justify-center items-center cursor-pointer px-4 h-full">
                             <div className="text-gray-900 font-semibold">
